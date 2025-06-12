4e05c013848c5135e447e8a334b59d60ee2b365f
tobias-barakaa <ngairato@gmail.com>
2025-06-12 13:14:50 +0300

    graphql GET handle Google Dev TOOLS?? GET REQUEST return {}

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/.well-known/appspecific/com.chrome.devtools.json')
+  chromeDevTools() {
+    return {}; // or return some DevTools configuration if needed
   }
 }
