be9c3db1fb3bfe907dae5cfa7378c0541630ff9a
tobias-barakaa <ngairato@gmail.com>
2025-06-22 10:54:20 +0300

    entity

--- src/school-type/resolvers/school-type.resolver.ts
+++ src/school-type/resolvers/school-type.resolver.ts
@@ -68,8 +68,8 @@ export class SchoolTypeResolver {
 
   private extractSubdomain(request: any): string {
     // Extract subdomain from Host header
-    // const host = request?.headers?.host || request?.headers?.Host || "sawa.squl.co.ke";
-    const host = "sawa-2.squl.co.ke"
+    const host = request?.headers?.host || request?.headers?.Host || "sawa.squl.co.ke";
+    // const host = "sawa-2.squl.co.ke"
     
     console.log('Host:', host);
     
