f8e5e928c27b6f6fed874a0381636e37ab6ab286
tobias-barakaa <ngairato@gmail.com>
2025-07-27 09:44:33 +0300

    redis

--- src/messaging/messaging.module.ts
+++ src/messaging/messaging.module.ts
@@ -16,10 +16,15 @@ import { ParentModule } from 'src/admin/parent/parent.module';
 @Module({
   imports: [
     TypeOrmModule.forFeature([ChatRoom, ChatMessage]),
+    // RedisModule.forRoot({
+    //   type: 'single',
+    //   url: process.env.REDIS_URL || 'redis://localhost:6379',
+    // }),
+
     RedisModule.forRoot({
-      type: 'single',
-      url: process.env.REDIS_URL || 'redis://localhost:6379',
-    }),
+  type: 'single',
+  url: process.env.REDIS_URL || 'redis://redis:6379',   // <-- use service name
+})
     StudentModule,
     ParentModule
   ],
