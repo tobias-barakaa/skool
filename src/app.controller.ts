4b9d8accfa6f1b813d60334c7bef2b2e8ac9ac15
tobias-barakaa <ngairato@gmail.com>
2025-11-28 14:03:28 +0300

    devtools

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,15 +17,9 @@ export class AppController {
     };
   }
 
-  @Get('/.well-known/appspecific/com.chrome.devtools.json')
-  chromeDevTools() {
-    return {};
-  }
-
-
-  @Get('health')
-  health() {
-    return { status: 'ok', timestamp: new Date().toISOString() };
-  }
+  // @Get('/.well-known/appspecific/com.chrome.devtools.json')
+  // chromeDevTools() {
+  //   return {};
+  // }
 }
 // EbtKKqozQ7IBCEF
